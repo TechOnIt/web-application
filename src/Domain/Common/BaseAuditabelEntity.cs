a18395e5951fa6b3eeb2a56b2d3a4c8b42a35957
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-10 20:33:13 +0430

    ♻️ Refactor

--- src/Domain/Common/BaseAuditabelEntity.cs
+++ src/Domain/Common/BaseAuditabelEntity.cs
@@ -5,9 +5,9 @@ namespace iot.Domain.Entities.Common
     public abstract class BaseAuditabelEntity 
     {
         public DateTime CreatedDateTime { get; set; }
-        public Guid? CreatedBy { get; set; } // null abel
+        public Guid? CreatedBy { get; set; } // nullabel
 
-        public DateTime? ModifyDateTime { get; set; } // null abel
-        public Guid? ModifyBy { get; set; } // null abel
+        public DateTime? ModifyDateTime { get; set; } // nullabel
+        public Guid? ModifyBy { get; set; } // nullabel
     }
 }

d3c1c7d77a42add82b080fd7228f0cf3c64d9876
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-10 19:43:22 +0430

    ♻️ Refactor user constructors. 📦 Add confirmed boolean.

--- src/Domain/Entities/Identity/User.cs
+++ src/Domain/Entities/Identity/User.cs
@@ -6,40 +6,39 @@ namespace iot.Domain.Entities.Identity
 {
     public class User
     {
-        public User(string username, string email, string password)
+        #region Constructors
+        User() { }
+
+        public User(string email, string phoneNumber, string password,
+            Guid? id = null, string surname = null, string name = null)
         {
-            Username = username;
-            Email = email;
-            Password = EncodePassword(password);
+            Id = id ?? Guid.NewGuid();
+            Username = phoneNumber;
+            Email = email.ToLower().Trim();
+            ConfirmedEmail = false;
+            PhoneNumber = phoneNumber;
+            ConfirmedPhoneNumber = false;
+            Password = EncodePassword(password); // Encode password as MD5.
+
+            Name = name;
+            Surname = surname;
             MaxFailCount = 0;
             RegisteredDateTime = DateTime.Now;
             IsBaned = false;
             IsDeleted = false;
         }
-
-        public User(Guid id, string username, string email, string password, string name, string surname,string phoneNumber)
-        {
-            Id = id;
-            Username = username;
-            Password = EncodePassword(password);
-            Email = email;
-            PhoneNumber = phoneNumber;
-            Name = name;
-            Surname = surname;
-            MaxFailCount = 0;
-            RegisteredDateTime = DateTime.Now;
-            IsBaned =   false;
-            IsDeleted = false;
-        }
+        #endregion
 
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public bool ConfirmedEmail { get; set; }
         public string Password { get; private set; }
         public DateTime RegisteredDateTime { get; private set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string PhoneNumber { get; set; }
+        public bool ConfirmedPhoneNumber { get; set; }
         public bool IsBaned { get; set; }
         public bool IsDeleted { get; set; }
         public short MaxFailCount { get; set; }

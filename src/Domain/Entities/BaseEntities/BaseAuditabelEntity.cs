084eeb4b956b55c410c986a27f745049e991e3d9
ashkan <ashnoori11@gmail.com>
2022-07-10 20:07:04 +0430

    📦 Built Login History Entity by Relation with user Entity 1-n

---
+++ src/Domain/Entities/BaseEntities/BaseAuditabelEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iot.Domain.Entities.BaseEntities
+{
+    public abstract class BaseAuditabelEntity 
+    {
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime  { get; set; }
+    }
+}

4ca35ee8e496a1f17796006cb3db87418b77df05
ashkan <ashnoori11@gmail.com>
2022-07-10 20:22:21 +0430

    📦 Built BaseAuditabelEntity as base class for Entities

---
+++ src/Domain/Common/BaseAuditabelEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace iot.Domain.Entities.Common
+{
+    public abstract class BaseAuditabelEntity 
+    {
+        public DateTime CreatedDateTime { get; set; }
+        public Guid? CreatedBy { get; set; } // null abel
+
+        public DateTime? ModifyDateTime { get; set; } // null abel
+        public Guid? ModifyBy { get; set; } // null abel
+    }
+}

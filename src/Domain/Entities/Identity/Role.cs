6b17c6aa7357f31de612869fc68df9d75dd24409
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-10 19:44:20 +0430

    🌱 Add role entity and constructors.

---
+++ src/Domain/Entities/Identity/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace iot.Domain.Entities.Identity
+{
+    public class Role
+    {
+        #region
+        Role() { }
+
+        public Role(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            NormalizedName = name.ToLower();
+        }
+        #endregion
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string NormalizedName { get; set; }
+    }
+}

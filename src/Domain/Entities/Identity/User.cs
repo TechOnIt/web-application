474f29dea75c577528b4eebc3db4e1abb19cd3c9
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-10 20:30:13 +0430

    🌱 Add set password for user.

--- src/Domain/Entities/Identity/User.cs
+++ src/Domain/Entities/Identity/User.cs
@@ -19,6 +19,7 @@ namespace iot.Domain.Entities.Identity
             PhoneNumber = phoneNumber;
             ConfirmedPhoneNumber = false;
             Password = EncodePassword(password); // Encode password as MD5.
+            ConcurrencyStamp = 
 
             Name = name;
             Surname = surname;
@@ -26,6 +27,8 @@ namespace iot.Domain.Entities.Identity
             RegisteredDateTime = DateTime.Now;
             IsBaned = false;
             IsDeleted = false;
+
+            GenerateSecurityStamp();
         }
         #endregion
 
@@ -39,10 +42,21 @@ namespace iot.Domain.Entities.Identity
         public string Surname { get; set; }
         public string PhoneNumber { get; set; }
         public bool ConfirmedPhoneNumber { get; set; }
+        public string ConcurrencyStamp { get; private set; }
         public bool IsBaned { get; set; }
         public bool IsDeleted { get; set; }
         public short MaxFailCount { get; set; }
         public DateTime? LockOutDateTime { get; set; }
+
+        public void GenerateSecurityStamp()
+        {
+            ConcurrencyStamp = Guid.NewGuid().ToString("N").Substring(0, 10);
+        }
+
+        public void SetPassword(string password)
+        {
+            Password = password;
+        }
 
         private string EncodePassword(string Password)
         {

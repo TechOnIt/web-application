ef1712e9f170336b0e50639e33d428b18f58b9d1
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-11 23:03:13 +0430

    🌱 Add PasswordHash logic & validations. 🧪 Pass the test of PasswordHash.

--- src/Domain/ValueObjects/PasswordHash.cs
+++ src/Domain/ValueObjects/PasswordHash.cs
@@ -1,10 +1,23 @@
-﻿namespace iot.Domain.ValueObjects
+﻿using System;
+
+namespace iot.Domain.ValueObjects
 {
     public class PasswordHash
     {
+        // Minimum password character length.
+        private int _minimumLength = 6;
+        // Maximum password character length.
+        private int _maximumLength = 50;
+
         PasswordHash() { }
         PasswordHash(string password)
         {
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException("Password cannot be null or empty.");
+            if (password.Length < _minimumLength)
+                throw new ArgumentOutOfRangeException($"Password characters cannot be less than {_minimumLength}.");
+            if (password.Length > _maximumLength)
+                throw new ArgumentOutOfRangeException($"Password characters cannot be more than {_maximumLength}.");
             Value = password;
         }
 

86697db4f6dc5eb564bcadbea6942dc3c42a1942
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-11 23:02:03 +0430

    ♻️ Add more theory. ♻️ Remove extra using.

--- Domain.UnitTests/ValueObjects/PasswordHashTests.cs
+++ Domain.UnitTests/ValueObjects/PasswordHashTests.cs
@@ -1,22 +1,19 @@
 ﻿using iot.Domain.ValueObjects;
 using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace iot.Domain.UnitTests.ValueObjects
 {
     public class PasswordHashTests
     {
+        #region Correct
         [Theory]
         [InlineData("123456")]
         [InlineData("aaaaaa")]
         [InlineData("QQQQQQ")]
         [InlineData("Aa1234")]
         [InlineData("@#$Fg5")]
+        [InlineData("pT6ec$5#cdA")]
         public void Password_Should_Can_Set_By_Parse(string password)
         {
             // Arrange
@@ -25,9 +22,14 @@ namespace iot.Domain.UnitTests.ValueObjects
             // Assert
             passwordHash.Value.ShouldNotBeNull();
         }
+        #endregion
 
+        #region error & exception
         [Theory]
+        [InlineData(null)]
+        [InlineData("abc")]
         [InlineData("1234")]
+        [InlineData("a%H5s")]
         public void Password_Should_Not_Be_Less_Than_Six(string password)
         {
             var exception = Record.Exception(() => PasswordHash.Parse(password));
@@ -37,6 +39,8 @@ namespace iot.Domain.UnitTests.ValueObjects
         }
 
         [Theory]
+        [InlineData("12345678912345678912345678912345678912345678912345u")]
+        [InlineData("Mcbjhger%275sgsDgef^df%2kf88nLgr*sa7kcbds5DfDj3bDfaefevffbr")]
         [InlineData("123456789123456789123456789123456789123456789123456789123456789123456789")]
         public void Password_Should_Not_Be_More_Than_Fifty(string password)
         {
@@ -45,5 +49,6 @@ namespace iot.Domain.UnitTests.ValueObjects
             Assert.NotNull(exception);
             Assert.NotNull(exception.Message);
         }
+        #endregion
     }
 }

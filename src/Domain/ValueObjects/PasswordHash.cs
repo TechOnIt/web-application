0e9963142850bac84c77b222715575b574c5834a
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-11 21:56:23 +0430

    🌱 Add PasswordHash value object without logics.

---
+++ src/Domain/ValueObjects/PasswordHash.cs
@@ -0,0 +1,18 @@
+﻿namespace iot.Domain.ValueObjects
+{
+    public class PasswordHash
+    {
+        PasswordHash() { }
+        PasswordHash(string password)
+        {
+            Value = password;
+        }
+
+        public string Value { get; private set; }
+
+        public static PasswordHash Parse(string password)
+        {
+            return new PasswordHash(password);
+        }
+    }
+}

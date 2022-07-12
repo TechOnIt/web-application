0448a54916777e4593b47dbcfd720c63fb900053
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-12 21:07:17 +0430

    ♻️ Refactor tests methods name.

--- tests/Domain.UnitTests/ValueObjects/PasswordHashTests.cs
+++ tests/Domain.UnitTests/ValueObjects/PasswordHashTests.cs
@@ -14,7 +14,7 @@ namespace iot.Domain.UnitTests.ValueObjects
         [InlineData("Aa1234")]
         [InlineData("@#$Fg5")]
         [InlineData("pT6ec$5#cdA")]
-        public void Password_Should_Can_Set_By_Parse(string password)
+        public void Should_Can_Set_By_Parse(string password)
         {
             // Arrange
             var passwordHash = PasswordHash.Parse(password);
@@ -45,7 +45,7 @@ namespace iot.Domain.UnitTests.ValueObjects
         [InlineData("abc")]
         [InlineData("1234")]
         [InlineData("a%H5s")]
-        public void Password_Should_Not_Be_Less_Than_Six(string password)
+        public void Should_Not_Be_Less_Than_Six(string password)
         {
             var exception = Record.Exception(() => PasswordHash.Parse(password));
 
@@ -57,7 +57,7 @@ namespace iot.Domain.UnitTests.ValueObjects
         [InlineData("12345678912345678912345678912345678912345678912345u")]
         [InlineData("Mcbjhger%275sgsDgef^df%2kf88nLgr*sa7kcbds5DfDj3bDfaefevffbr")]
         [InlineData("123456789123456789123456789123456789123456789123456789123456789123456789")]
-        public void Password_Should_Not_Be_More_Than_Fifty(string password)
+        public void Should_Not_Be_More_Than_Fifty(string password)
         {
             var exception = Record.Exception(() => PasswordHash.Parse(password));
 

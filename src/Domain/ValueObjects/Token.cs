dd6f398193078618685c6a8812533bbf2ea0c759
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-15 02:48:10 +0430

    🌱 Add Token value object. 🧪 Pass the test.

---
+++ src/Domain/ValueObjects/Token.cs
@@ -0,0 +1,28 @@
+﻿using iot.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace iot.Domain.ValueObjects;
+
+public class Token : ValueObject
+{
+    #region Constructors
+    Token() { }
+    #endregion
+
+    public string Value { get; private set; }
+
+    #region Methods
+    public static Token CreateNew()
+    {
+        var instance = new Token();
+        instance.Value = Guid.NewGuid().ToString("N").Substring(0, 12);
+        return instance;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+    #endregion
+}

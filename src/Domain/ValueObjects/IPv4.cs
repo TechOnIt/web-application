6473c529285d0cf42a8ee989e7f122358f09749a
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-15 02:03:13 +0430

    🌱 Init private validation method to IPv4 value object.

--- src/Domain/ValueObjects/IPv4.cs
+++ src/Domain/ValueObjects/IPv4.cs
@@ -1,22 +1,39 @@
-﻿using System;
+﻿using iot.Domain.Common;
+using System;
+using System.Collections.Generic;
 
 namespace iot.Domain.ValueObjects;
 
-public class IPv4
+public class IPv4 : ValueObject
 {
     #region Constructors
     public IPv4() { }
 
     public IPv4(string address)
     {
-        Address = address;
+        Address = _validation(address);
     }
     #endregion
 
     public string Address { get; private set; }
 
+    #region Methods
     public static IPv4 Parse(string address)
     {
         return new IPv4(address);
     }
+
+    private static string _validation(string ipAddress)
+    {
+        // Validation
+
+
+        return ipAddress;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Address;
+    }
+    #endregion
 }

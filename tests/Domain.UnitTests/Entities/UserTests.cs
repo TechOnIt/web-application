78e44759fffa417c2c4dc5c1b8f1001e0fba56e8
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-15 01:49:34 +0430

    ♻️ Refactor namespace to c# 10.

--- tests/Domain.UnitTests/Entities/UserTests.cs
+++ tests/Domain.UnitTests/Entities/UserTests.cs
@@ -3,39 +3,38 @@ using iot.Domain.Entities.Identity;
 using System;
 using Xunit;
 
-namespace iot.Domain.UnitTests.Entities
+namespace iot.Domain.UnitTests.Entities;
+
+public class UserTests
 {
-    public class UserTests
+    private readonly User model;
+
+    public UserTests()
     {
-        private readonly User model;
+        model = new User(email: "test@gmail.com", phoneNumber: "09124133486", password: "123456",
+            name: "testName", surname: "testSurname");
+    }
 
-        public UserTests()
-        {
-            model = new User(email: "test@gmail.com", phoneNumber: "09124133486", password: "123456",
-                name: "testName", surname: "testSurname");
-        }
+    [Fact]
+    public void UserName_Should_Equal_To_PhoneNumber()
+    {
+        // Arrange
+        var user = new User();
 
-        [Fact]
-        public void UserName_Should_Equal_To_PhoneNumber()
-        {
-            // Arrange
-            var user = new User();
+        // Act
+        user.PhoneNumber = model.PhoneNumber;
 
-            // Act
-            user.PhoneNumber = model.PhoneNumber;
+        // Assert
+        user.Username.Should().Be(user.PhoneNumber);
+    }
 
-            // Assert
-            user.Username.Should().Be(user.PhoneNumber);
-        }
+    [Fact]
+    public void Id_Should_Not_Be_Null_If_Not_Define_In_Constructor()
+    {
+        // Arrange
+        var user = new User();
 
-        [Fact]
-        public void Id_Should_Not_Be_Null_If_Not_Define_In_Constructor()
-        {
-            // Arrange
-            var user = new User();
-
-            // Assert
-            user.Id.Should().NotBe(Guid.Empty);
-        }
+        // Assert
+        user.Id.Should().NotBe(Guid.Empty);
     }
 }
--- tests/Domain.UnitTests/ValueObjects/PasswordHashTests.cs
+++ tests/Domain.UnitTests/ValueObjects/PasswordHashTests.cs
@@ -2,82 +2,81 @@
 using iot.Domain.ValueObjects;
 using Xunit;
 
-namespace iot.Domain.UnitTests.ValueObjects
+namespace iot.Domain.UnitTests.ValueObjects;
+
+public class PasswordHashTests
 {
-    public class PasswordHashTests
+    #region Correct
+    [Theory]
+    [InlineData("123456")]
+    [InlineData("aaabbb")]
+    [InlineData("QQQQQQ")]
+    [InlineData("Aa1234")]
+    [InlineData("@#$Fg5")]
+    [InlineData("pT6ec$5#cdA")]
+    public void Should_Can_Set_By_Parse(string password)
     {
-        #region Correct
-        [Theory]
-        [InlineData("123456")]
-        [InlineData("aaabbb")]
-        [InlineData("QQQQQQ")]
-        [InlineData("Aa1234")]
-        [InlineData("@#$Fg5")]
-        [InlineData("pT6ec$5#cdA")]
-        public void Should_Can_Set_By_Parse(string password)
-        {
-            // Arrange
-            var passwordHash = PasswordHash.Parse(password);
+        // Arrange
+        var passwordHash = PasswordHash.Parse(password);
 
-            // Assert
-            passwordHash.Value.Should().NotBe(null);
-        }
-
-        [Theory]
-        [InlineData("123456", "123456")]
-        [InlineData("abcDefg", "abcDefg")]
-        [InlineData("aB8%h*9T", "aB8%h*9T")]
-        public void Should_Equal_With_Same_Type_And_Same_Value(string oldPassword, string newPassword)
-        {
-            // Arrange
-            var oldPasswordHash = PasswordHash.Parse(oldPassword);
-            var newPasswordHash = PasswordHash.Parse(newPassword);
-
-            // Assert
-            oldPasswordHash.Should().Match<PasswordHash>(passwordHash => passwordHash == newPasswordHash);
-            oldPasswordHash.Should().Match<PasswordHash>(passwordHash => passwordHash.Equals(newPasswordHash));
-        }
-        #endregion
-
-        #region error & exception
-        [Theory]
-        [InlineData(null)]
-        [InlineData("abc")]
-        [InlineData("1234")]
-        [InlineData("a%H5s")]
-        public void Should_Not_Be_Less_Than_Six(string password)
-        {
-            var exception = Record.Exception(() => PasswordHash.Parse(password));
-
-            Assert.NotNull(exception);
-            Assert.NotNull(exception.Message);
-        }
-
-        [Theory]
-        [InlineData("12345678912345678912345678912345678912345678912345u")]
-        [InlineData("Mcbjhger%275sgsDgef^df%2kf88nLgr*sa7kcbds5DfDj3bDfaefevffbr")]
-        [InlineData("123456789123456789123456789123456789123456789123456789123456789123456789")]
-        public void Should_Not_Be_More_Than_Fifty(string password)
-        {
-            var exception = Record.Exception(() => PasswordHash.Parse(password));
-
-            Assert.NotNull(exception);
-            Assert.NotNull(exception.Message);
-        }
-
-        [Theory]
-        [InlineData("sdkoeh", "nkjbrS")]
-        public void Should_Not_Equal_With_Diffrent_Value(string oldPassword, string newPassword)
-        {
-            // Arrange
-            var oldPasswordHash = PasswordHash.Parse(oldPassword);
-            var newPasswordHash = PasswordHash.Parse(newPassword);
-
-            // Assert
-            oldPasswordHash.Should().Match<PasswordHash>(passwordHash => passwordHash != newPasswordHash);
-            oldPasswordHash.Should().Match<PasswordHash>(passwordHash => !passwordHash.Equals(newPasswordHash));
-            oldPasswordHash.Should().Match<PasswordHash>(passwordHash => passwordHash.NotEquals(newPasswordHash));
-        }
-        #endregion
+        // Assert
+        passwordHash.Value.Should().NotBe(null);
     }
+
+    [Theory]
+    [InlineData("123456", "123456")]
+    [InlineData("abcDefg", "abcDefg")]
+    [InlineData("aB8%h*9T", "aB8%h*9T")]
+    public void Should_Equal_With_Same_Type_And_Same_Value(string oldPassword, string newPassword)
+    {
+        // Arrange
+        var oldPasswordHash = PasswordHash.Parse(oldPassword);
+        var newPasswordHash = PasswordHash.Parse(newPassword);
+
+        // Assert
+        oldPasswordHash.Should().Match<PasswordHash>(passwordHash => passwordHash == newPasswordHash);
+        oldPasswordHash.Should().Match<PasswordHash>(passwordHash => passwordHash.Equals(newPasswordHash));
+    }
+    #endregion
+
+    #region error & exception
+    [Theory]
+    [InlineData(null)]
+    [InlineData("abc")]
+    [InlineData("1234")]
+    [InlineData("a%H5s")]
+    public void Should_Not_Be_Less_Than_Six(string password)
+    {
+        var exception = Record.Exception(() => PasswordHash.Parse(password));
+
+        Assert.NotNull(exception);
+        Assert.NotNull(exception.Message);
+    }
+
+    [Theory]
+    [InlineData("12345678912345678912345678912345678912345678912345u")]
+    [InlineData("Mcbjhger%275sgsDgef^df%2kf88nLgr*sa7kcbds5DfDj3bDfaefevffbr")]
+    [InlineData("123456789123456789123456789123456789123456789123456789123456789123456789")]
+    public void Should_Not_Be_More_Than_Fifty(string password)
+    {
+        var exception = Record.Exception(() => PasswordHash.Parse(password));
+
+        Assert.NotNull(exception);
+        Assert.NotNull(exception.Message);
+    }
+
+    [Theory]
+    [InlineData("sdkoeh", "nkjbrS")]
+    public void Should_Not_Equal_With_Diffrent_Value(string oldPassword, string newPassword)
+    {
+        // Arrange
+        var oldPasswordHash = PasswordHash.Parse(oldPassword);
+        var newPasswordHash = PasswordHash.Parse(newPassword);
+
+        // Assert
+        oldPasswordHash.Should().Match<PasswordHash>(passwordHash => passwordHash != newPasswordHash);
+        oldPasswordHash.Should().Match<PasswordHash>(passwordHash => !passwordHash.Equals(newPasswordHash));
+        oldPasswordHash.Should().Match<PasswordHash>(passwordHash => passwordHash.NotEquals(newPasswordHash));
+    }
+    #endregion
 }

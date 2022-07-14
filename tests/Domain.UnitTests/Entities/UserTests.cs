f91aca8702b2dd15b5a00b17e9cabb3a23866ded
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-15 01:47:33 +0430

    🚀 Upgrade domain test to dotnet 6.

--- tests/Domain.UnitTests/Entities/UserTests.cs
+++ tests/Domain.UnitTests/Entities/UserTests.cs
@@ -11,18 +11,8 @@ namespace iot.Domain.UnitTests.Entities
 
         public UserTests()
         {
-            model = new User
-            {
-                Email = "test@gmail.com",
-                PhoneNumber = "09124133486",
-                Name = "testName",
-                Surname = "testSurname",
-                ConfirmedPhoneNumber = false,
-                MaxFailCount = 0,
-                ConfirmedEmail = false,
-                IsBaned = false,
-                IsDeleted = false,
-            };
+            model = new User(email: "test@gmail.com", phoneNumber: "09124133486", password: "123456",
+                name: "testName", surname: "testSurname");
         }
 
         [Fact]

08576ffbc482a8668e125d67c837535a5a2def76
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-15 02:47:41 +0430

    🧪 Test Token value object.

---
+++ tests/Domain.UnitTests/ValueObjects/TokenTests.cs
@@ -0,0 +1,26 @@
+﻿using iot.Domain.ValueObjects;
+
+namespace iot.Domain.UnitTests.ValueObjects;
+
+public class TokenTests
+{
+    [Fact]
+    public void Should_not_be_null()
+    {
+        // Arrange
+        var token = Token.CreateNew();
+
+        // Assert
+        token.Value.Should().NotBeNullOrEmpty(token.Value);
+    }
+
+    [Fact]
+    public void Should_have_12_character()
+    {
+        // Arrange
+        var token = Token.CreateNew();
+
+        // Assert
+        token.Value.Should().HaveLength(12);
+    }
+}

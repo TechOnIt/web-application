0742edcff7dc6e73aa7cd855cfe79041dc5c8edc
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-15 22:33:03 +0430

    🌱 Initialize login history MediatR.

---
+++ src/Application/Common/Services/LoginHistory/Create/LoginHistoryCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using iot.Domain.ValueObjects;
+using MediatR;
+
+namespace iot.Application.Common.Services.LoginHistory.Create;
+
+public class LoginHistoryCreateCommand : IRequest
+{
+    public IPv4 Ip { get; set; }
+}
---
+++ src/Application/Common/Services/LoginHistory/Create/LoginHistoryCreateCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iot.Application.Common.Services.LoginHistory.Create
+{
+    public class LoginHistoryCreateCommandHandler : IRequestHandler<LoginHistoryCreateCommand>
+    {
+        public Task<Unit> Handle(LoginHistoryCreateCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

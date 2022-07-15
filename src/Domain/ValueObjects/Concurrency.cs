ad86853afe490a221d5c6f0f2567be3a0878c018
ashkan noori <ashnoori11@gmail.com>
2022-07-15 03:29:15 -0700

    🧪 Added Concurrency ValueObject and its tests as ConcurrencyTests in Xunit Solution

---
+++ src/Domain/ValueObjects/Concurrency.cs
@@ -0,0 +1,37 @@
+﻿using iot.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iot.Domain.ValueObjects
+{
+    public class Concurrency : ValueObject
+    {
+        #region constructure
+        public Concurrency()
+        {
+
+        }
+        #endregion
+
+        public static string Value { get; set; }
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789qwertyuiopasdfghjklzxcvbnm";
+
+        public static string NewToken()
+        {
+            var random = new Random();
+
+            Value= new string(Enumerable.Repeat(chars, 16)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            return Value;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
---
+++ tests/Domain.UnitTests/ValueObjects/ConcurrencyTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using iot.Domain.ValueObjects;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iot.Domain.UnitTests.ValueObjects
+{
+    public class ConcurrencyTests
+    {
+        [Fact]
+        public void Should_Not_Be_Null_Or_Empty()
+        {
+            var resultConcurrency = Concurrency.NewToken();
+
+            Assert.NotEmpty(resultConcurrency);
+            Assert.NotNull(resultConcurrency);
+        }
+
+        [Fact]
+        public void Should_Token_Has_Sixteen_Characters()
+        {
+            var resultConcurrency = Concurrency.NewToken();
+
+            Assert.True(resultConcurrency.Length <17);
+            Assert.True(resultConcurrency.Length >15);
+        }
+
+
+        //[Theory]
+        //[MemberData(nameof(TokenTest))]
+        //public static IEnumerable<object[]> TokenTest()
+        //{
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        yield return new object[] { Concurrency.NewToken() };
+        //        yield return new object[] { Concurrency.NewToken() };
+        //        yield return new object[] { Concurrency.NewToken() };
+        //    }
+        //}
+    }
+}

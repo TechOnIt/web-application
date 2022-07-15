d07ec8e5e726141666251f5f4a270fc723ff37d1
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-15 22:35:31 +0430

    🌱 Add identity api layout & inslatt MediatR.

---
+++ src/Presentations/Identity.Api/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using iot.Application.Common.Services.LoginHistory.Create;
+using iot.Domain.ValueObjects;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iot.Identity.Api.Controllers;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public HomeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Test()
+    {
+        var createLoginHistoryResult = await _mediator.Send(new LoginHistoryCreateCommand
+        {
+            Ip = IPv4.Parse("192.168.1.100")
+        });
+
+        return Ok();
+    }
+}
---
+++ src/Presentations/Identity.Api/Program.cs
@@ -0,0 +1,39 @@
+using iot.Application.Common.Services.LoginHistory.Create;
+using MediatR;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddMediatR(typeof(LoginHistoryCreateCommand).GetTypeInfo().Assembly);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=General}/{action=Index}/{id?}");
+
+    endpoints.MapControllers();
+});
+await app.RunAsync();

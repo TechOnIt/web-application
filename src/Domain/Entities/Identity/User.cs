01bb1de646268a888308dd35e74179f6c46e5093
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-18 20:40:58 +0430

    ♻️ Refactor namespace.

--- src/Domain/Entities/Identity/User.cs
+++ src/Domain/Entities/Identity/User.cs
@@ -1,87 +1,86 @@
 ﻿using iot.Domain.ValueObjects;
 using System;
 
-namespace iot.Domain.Entities.Identity
+namespace iot.Domain.Entities.Identity;
+
+public class User
 {
-    public class User
+    #region Constructors
+    User() { }
+
+    public User(string email, string phoneNumber,
+        PasswordHash passwordHash = null, string surname = null, string name = null)
     {
-        #region Constructors
-        User() { }
+        Id = Guid.NewGuid();
+        SetEmail(email);
+        SetPhoneNumber(phoneNumber); // Also set phone number in username.
+        Password = passwordHash;
 
-        public User(string email, string phoneNumber,
-            PasswordHash passwordHash = null, string surname = null, string name = null)
-        {
-            Id = Guid.NewGuid();
-            SetEmail(email);
-            SetPhoneNumber(phoneNumber); // Also set phone number in username.
-            Password = passwordHash;
-
-            Name = name;
-            Surname = surname;
-            MaxFailCount = 0;
-            RegisteredDateTime = DateTime.Now;
-            IsBaned = false;
-            IsDeleted = false;
-            ConcurrencyStamp = Token.CreateNew(); // Create new stamp.
-        }
-        #endregion
-
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string Username { get; private set; }
-        public PasswordHash Password { get; private set; } // Must be nullable. maybe we use otp in future!
-        public string Email { get; private set; }
-        public bool ConfirmedEmail { get; private set; }
-        public string PhoneNumber { get; private set; }
-        public bool ConfirmedPhoneNumber { get; private set; }
-
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public DateTime RegisteredDateTime { get; private set; }
-        public Token ConcurrencyStamp { get; private set; }
-        public bool IsBaned { get; set; }
-        public bool IsDeleted { get; set; }
-        public short MaxFailCount { get; private set; }
-        public DateTime? LockOutDateTime { get; private set; }
-
-        #region Methods
-        public void SetEmail(string email)
-        {
-            Email = email.Trim().ToLower();
-            ConfirmedEmail = false;
-        }
-        public void ConfirmEmail()
-        {
-            if (string.IsNullOrEmpty(Email))
-                throw new ArgumentNullException("While the email is empty, it cannot be verified.");
-            ConfirmedEmail = true;
-        }
-        public void SetPhoneNumber(string phoneNumber)
-        {
-            PhoneNumber = phoneNumber;
-            Username = phoneNumber;
-            ConfirmedPhoneNumber = false;
-        }
-        public void ConfirmPhoneNumber()
-        {
-            if (string.IsNullOrEmpty(PhoneNumber))
-                throw new ArgumentNullException("While the phone number is empty, it cannot be verified.");
-            ConfirmedPhoneNumber = true;
-        }
-        public void SetLockOut(DateTime lockoutUntill)
-        {
-            if (lockoutUntill <= DateTime.Now)
-                throw new ArgumentOutOfRangeException("lockout cannot be in the past or present!");
-            LockOutDateTime = lockoutUntill;
-        }
-        public void UnLock()
-        {
-            MaxFailCount = 0;
-            LockOutDateTime = null;
-        }
-        public void IncreaseMaxFailCount()
-        {
-            MaxFailCount++;
-        }
-        #endregion
+        Name = name;
+        Surname = surname;
+        MaxFailCount = 0;
+        RegisteredDateTime = DateTime.Now;
+        IsBaned = false;
+        IsDeleted = false;
+        ConcurrencyStamp = Token.CreateNew(); // Create new stamp.
     }
+    #endregion
+
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Username { get; private set; }
+    public PasswordHash Password { get; private set; } // Must be nullable. maybe we use otp in future!
+    public string Email { get; private set; }
+    public bool ConfirmedEmail { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public bool ConfirmedPhoneNumber { get; private set; }
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime RegisteredDateTime { get; private set; }
+    public Token ConcurrencyStamp { get; private set; }
+    public bool IsBaned { get; set; }
+    public bool IsDeleted { get; set; }
+    public short MaxFailCount { get; private set; }
+    public DateTime? LockOutDateTime { get; private set; }
+
+    #region Methods
+    public void SetEmail(string email)
+    {
+        Email = email.Trim().ToLower();
+        ConfirmedEmail = false;
+    }
+    public void ConfirmEmail()
+    {
+        if (string.IsNullOrEmpty(Email))
+            throw new ArgumentNullException("While the email is empty, it cannot be verified.");
+        ConfirmedEmail = true;
+    }
+    public void SetPhoneNumber(string phoneNumber)
+    {
+        PhoneNumber = phoneNumber;
+        Username = phoneNumber;
+        ConfirmedPhoneNumber = false;
+    }
+    public void ConfirmPhoneNumber()
+    {
+        if (string.IsNullOrEmpty(PhoneNumber))
+            throw new ArgumentNullException("While the phone number is empty, it cannot be verified.");
+        ConfirmedPhoneNumber = true;
+    }
+    public void SetLockOut(DateTime lockoutUntill)
+    {
+        if (lockoutUntill <= DateTime.Now)
+            throw new ArgumentOutOfRangeException("lockout cannot be in the past or present!");
+        LockOutDateTime = lockoutUntill;
+    }
+    public void UnLock()
+    {
+        MaxFailCount = 0;
+        LockOutDateTime = null;
+    }
+    public void IncreaseMaxFailCount()
+    {
+        MaxFailCount++;
+    }
+    #endregion
 }

b7c55a6edeec71bd83d5d8a27209379d3ef2d814
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-18 20:42:48 +0430

    🌱 Add Identity DbContext.

---
+++ src/Application/Common/Services/IIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using iot.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace iot.Application.Common.Services;
+
+internal interface IIdentityDbContext
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+    DbSet<User> Users { get; set; }
+    DbSet<Role> Roles { get; set; }
+    DbSet<UserRole> UsersRoles { get; set; }
+    DbSet<LoginHistory> LoginHistories { get; set; }
+}
---
+++ src/Infrastructure/Persistence/Context/IdentityContext.cs
@@ -0,0 +1,9 @@
+﻿using iot.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace iot.Infrastructure.Persistence.Context;
+
+public class IdentityContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+}

d744f709d1bce19dc42dc693563a0afcbf6258c3
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-20 18:54:24 +0430

    🌱 Initialize JWT Service.

---
+++ src/Infrastructure/Common/JwtBearerService/JwtService.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace iot.Application.Services.JwtBearerService
+{
+    public class JwtService
+    {
+        /// <summary>
+        /// Generate JWT Token with claims.
+        /// </summary>
+        /// <param name="expireDateTime">
+        /// Expire date & time.
+        /// </param>
+        public void GenerateTokenWithClaims(List<Claim> claims, DateTime expireDateTime = default)
+        {
+
+        }
+    }
+}

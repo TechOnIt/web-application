c23c095ee1c499770f9b982cd314f011a14251f2
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-20 18:26:54 +0430

    🌱 Add Configure Service to infrastructure layer.

---
+++ src/Infrastructure/ConfigureService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace iot.Infrastructure;
+
+public static class ConfigureService
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+
+
+        return services;
+    }
+}

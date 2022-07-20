73c1ea918c2dff9df70572b5304e30b92ce8351d
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-20 19:31:43 +0430

    ♻️ Remove extra usings.

--- src/Application/Common/Services/LoginHistoryService/Create/LoginHistoryCreateCommandHandler.cs
+++ src/Application/Common/Services/LoginHistoryService/Create/LoginHistoryCreateCommandHandler.cs
@@ -1,18 +1,12 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace iot.Application.Common.Services.LoginHistoryService.Create
+namespace iot.Application.Common.Services.LoginHistoryService.Create;
+
+public class LoginHistoryCreateCommandHandler : IRequestHandler<LoginHistoryCreateCommand>
 {
-    public class LoginHistoryCreateCommandHandler : IRequestHandler<LoginHistoryCreateCommand>
-    {
 
-        public Task<Unit> Handle(LoginHistoryCreateCommand request, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<Unit> Handle(LoginHistoryCreateCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
     }
 }

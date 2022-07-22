0e3695f650a2941123b7914eca853208706d9596
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-22 12:37:05 +0430

    🌱 Initialize FullName value object.

---
+++ src/Domain/ValueObjects/FullName.cs
@@ -0,0 +1,19 @@
+﻿namespace iot.Domain.ValueObjects;
+
+public class FullName
+{
+    public FullName() { }
+
+    public string Name { get; set; } // First name
+    public string Surname { get; set; } // Last name
+
+    public void SetName(string name)
+    {
+
+    }
+
+    public void SetSurname(string surname)
+    {
+
+    }
+}

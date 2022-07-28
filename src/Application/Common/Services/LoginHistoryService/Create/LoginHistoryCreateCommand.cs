83bd9b69533b72e3f6a009a77e5773e06b515008
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-28 16:33:36 +0430

    🌱 Validate login history command.

--- src/Application/Common/Services/LoginHistoryService/Create/LoginHistoryCreateCommand.cs
+++ src/Application/Common/Services/LoginHistoryService/Create/LoginHistoryCreateCommand.cs
@@ -1,9 +1,23 @@
-﻿using iot.Domain.ValueObjects;
+﻿using FluentValidation;
+using iot.Application.Models;
 using MediatR;
 
 namespace iot.Application.Common.Services.LoginHistoryService.Create;
 
 public class LoginHistoryCreateCommand : IRequest
 {
-    public IPv4 Ip { get; set; }
+    public string Ip { get; set; }
+}
+
+public class LoginHistoryCreateCommandValidator : BaseFluentValidator<LoginHistoryCreateCommand>
+{
+    public LoginHistoryCreateCommandValidator()
+    {
+        When(loginHistory => string.IsNullOrEmpty(loginHistory.Ip), () =>
+        {
+            RuleFor(loginHistory => loginHistory.Ip)
+                .NotEmpty()
+                .WithMessage("آی پی دستگاه اجباری است");
+        });
+    }
 }

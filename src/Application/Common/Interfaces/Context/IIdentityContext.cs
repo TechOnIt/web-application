252548f3f288a610845da17ceac1c22a8486d4f2
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-29 00:37:17 +0430

    🌱 Add context interface.

---
+++ src/Application/Common/Interfaces/Context/IIdentityContext.cs
@@ -0,0 +1,9 @@
+﻿using iot.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace iot.Application.Common.Interfaces.Context;
+
+public interface IIdentityContext
+{
+    public DbSet<User> Users { get; set; }
+}
--- src/Infrastructure/Persistence/Context/IdentityContext.cs
+++ src/Infrastructure/Persistence/Context/IdentityContext.cs
@@ -1,9 +1,10 @@
-﻿using iot.Domain.Entities.Identity;
+﻿using iot.Application.Common.Interfaces.Context;
+using iot.Domain.Entities.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace iot.Infrastructure.Persistence.Context;
 
-public class IdentityContext : DbContext
+public class IdentityContext : DbContext, IIdentityContext
 {
     public DbSet<User> Users { get; set; }
 }

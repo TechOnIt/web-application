66c206717573f79cb592c26ebd5135b352396450
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-28 16:31:24 +0430

    🌱 Add configure service for application layout.

---
+++ src/Application/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.AspNetCore;
+using iot.Application.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace iot.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services
+            // Fluent Validation
+            .AddFluentValidation(fv =>
+            {
+                fv.RegisterValidatorsFromAssemblyContaining<BaseFluentValidator<object>>();
+            })
+            ;
+        return services;
+    }
+}

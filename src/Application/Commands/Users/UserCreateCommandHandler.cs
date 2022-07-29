43c26ff6c1ac76e8b9ec62ac2cc0007791e4e05d
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-07-29 23:31:35 +0430

    ♻️ Use user repository in create command handler.
    Remove context injection & replace user repository.
    
    Co-Authored-By: Ashkan Noori <ashkandeveloper11@gmail.com>

--- src/Application/Commands/Users/UserCreateCommandHandler.cs
+++ src/Application/Commands/Users/UserCreateCommandHandler.cs
@@ -1,30 +1,25 @@
-﻿using iot.Application.Common.Interfaces.Context;
+﻿using iot.Application.Repositories.SQL.Users;
 using iot.Domain.Entities.Identity;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace iot.Application.Commands.Users
 {
     public class UserCreateCommandHandler : IRequestHandler<UserCreateCommand, Guid>
     {
-        #region constracture
-        public IIdentityContext _context { get; set; }
-        public UserCreateCommandHandler(IIdentityContext context)
+        #region DI & Ctor's
+        public IUserRepository _userRepository { get; set; }
+        public UserCreateCommandHandler(IUserRepository userRepository)
         {
-            _context = context;
+            _userRepository = userRepository;
         }
         #endregion
 
         public async Task<Guid> Handle(UserCreateCommand request, CancellationToken cancellationToken)
         {
-            var user = new User(request.Email,request.PhoneNumber,request.Password,request.Surname,request.Name);
-            var result= await _context.Users.AddAsync(user);
+            var user = new User(request.Email, request.PhoneNumber, request.Password, request.Surname, request.Name);
+            await _userRepository.AddAsync(user);
 
-            return result.Entity.Id;
+            return user.Id;
         }
     }
 }

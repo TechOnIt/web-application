936812adfad014141f1568cdbb7895a1c08c5952
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-07-30 00:29:01 +0430

    ♻️ Cleanup: remove spaces...

--- src/Domain/ValueObjects/FullName.cs
+++ src/Domain/ValueObjects/FullName.cs
@@ -16,9 +16,8 @@ public class FullName : ValueObject
     }
     #endregion
 
-
-    public string Name { get; set; } // First name
-    public string Surname { get; set; } // Last name
+    public string Name { get; set; }
+    public string Surname { get; set; }
 
     public string GetName() => Name;
     public string GetSurname() => Surname;

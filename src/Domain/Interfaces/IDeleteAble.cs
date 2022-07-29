e4c87d3d2c79e615282c2af7be5cd451b8cd8e48
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-07-30 01:18:40 +0430

    Add Interfaces to domain layout.

---
+++ src/Domain/Interfaces/IDeleteAble.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace iot.Domain.Interfaces;
+
+public interface IDeleteable
+{
+    public bool IsDeleted { get; set; }
+    public string? ModifyBy { get; set; }
+    public DateTime? LastModifiedOn { get; set; }
+
+}
---
+++ src/Domain/Interfaces/IEditAble.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace iot.Domain.Interfaces;
+
+internal interface IEditAble
+{
+    public string? ModifyBy { get; set; }
+    public DateTime? LastModifiedOn { get; set; }
+}

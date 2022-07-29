9991b5b621e433368d48660bb687c216f08a5d23
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-07-30 02:03:00 +0430

    🌱 Add generic interface for register scope life cycle.

---
+++ src/Application/Common/Interfaces/Dependency/IScopedDependency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iot.Application.Common.Interfaces.Dependency;
+
+internal interface IScopedDependency
+{
+
+}

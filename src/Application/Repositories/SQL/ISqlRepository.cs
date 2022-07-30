12c2357fdb2ad36b4ddaad2ac862cd846c296fee
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-07-30 22:39:28 +0430

    ♻️ Refactor & Code-Cleanup SQL base repositories.

--- src/Application/Repositories/SQL/ISqlRepository.cs
+++ src/Application/Repositories/SQL/ISqlRepository.cs
@@ -10,19 +10,26 @@ public interface ISqlRepository<TEntity> where TEntity : class, IEntity
     IQueryable<TEntity> Table { get; }
     IQueryable<TEntity> TableNoTracking { get; }
 
+    TEntity? GetById(params object[] id);
+    Task<TEntity?> GetByIdAsync(CancellationToken stoppingToken = default, params object[] id);
+    List<TEntity> GetAll(bool asNoTracking);
+    Task<List<TEntity>> GetAllAsync(bool asNoTracking, CancellationToken stoppingToken = default);
+
     void Add(TEntity entity, bool saveNow = true);
-    Task AddAsync(TEntity entity, bool saveNow = true, CancellationToken cancellationToken = default);
+    Task AddAsync(TEntity entity, bool saveNow = true, CancellationToken stoppingToken = default);
     void AddRange(IEnumerable<TEntity> entities, bool saveNow = true);
-    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
-    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
-    void DeleteRange(IEnumerable<TEntity> entities, bool saveNow = true);
-    Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
-    TEntity GetById(params object[] ids);
-    Task<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids);
+    Task AddRangeAsync(IEnumerable<TEntity> entities, bool saveNow = true, CancellationToken stoppingToken = default);
+
     void Update(TEntity entity, bool saveNow = true);
-    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
+    Task UpdateAsync(TEntity entity, bool saveNow = true, CancellationToken stoppingToken = default);
     void UpdateRange(IEnumerable<TEntity> entities, bool saveNow = true);
-    Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
-    Task<List<TEntity>> GetAllAsync(bool asNoTracking, CancellationToken cancellationToken);
-    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+    Task UpdateRangeAsync(IEnumerable<TEntity> entities, bool saveNow = true, CancellationToken stoppingToken = default);
+
+    void Delete(TEntity entity, bool saveNow = true);
+    Task DeleteAsync(TEntity entity, bool saveNow = true, CancellationToken stoppingToken = default);
+    void DeleteRange(IEnumerable<TEntity> entities, bool saveNow = true);
+    Task DeleteRangeAsync(IEnumerable<TEntity> entities, bool saveNow = true, CancellationToken stoppingToken = default);
+
+    bool IsExists(Expression<Func<TEntity, bool>> expression);
+    Task<bool> IsExistsAsync(Expression<Func<TEntity, bool>> expression, CancellationToken stoppingToken = default);
 }
--- src/Application/Repositories/SQL/SqlRepository.cs
+++ src/Application/Repositories/SQL/SqlRepository.cs
@@ -20,66 +20,9 @@ public class SqlRepository<TEntity> : ISqlRepository<TEntity>
         Entities = _context.Set<TEntity>();
     }
 
-    #region Async Method
-    public async virtual Task<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids)
-    {
-        return await Entities.FindAsync(ids, cancellationToken);
-    }
-
-    public virtual async Task AddAsync(TEntity entity, bool saveNow = true, CancellationToken cancellationToken = default)
-    {
-        Assert.NotNull(entity, nameof(entity));
-        await Entities.AddAsync(entity, cancellationToken).ConfigureAwait(false);
-        if (saveNow)
-            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-    }
-
-    public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
-    {
-        Assert.NotNull(entities, nameof(entities));
-        await Entities.AddRangeAsync(entities, cancellationToken).ConfigureAwait(false);
-        if (saveNow)
-            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-    }
-
-    public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
-    {
-        Assert.NotNull(entity, nameof(entity));
-        Entities.Update(entity);
-        if (saveNow)
-            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-    }
-
-    public virtual async Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
-    {
-        Assert.NotNull(entities, nameof(entities));
-        Entities.UpdateRange(entities);
-        if (saveNow)
-            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-    }
-
-    public virtual async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
-    {
-        Assert.NotNull(entity, nameof(entity));
-        Entities.Remove(entity);
-        if (saveNow)
-            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-    }
-
-    public virtual async Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
-    {
-        Assert.NotNull(entities, nameof(entities));
-        Entities.RemoveRange(entities);
-        if (saveNow)
-            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-    }
-    #endregion
-
     #region Sync Methods
-    public virtual TEntity GetById(params object[] ids)
-    {
-        return Entities.Find(ids);
-    }
+    public virtual TEntity? GetById(params object[] id) => Entities.Find(id);
+    public virtual List<TEntity> GetAll(bool asNoTracking) => (asNoTracking ? TableNoTracking : Entities).ToList();
 
     public virtual void Add(TEntity entity, bool saveNow = true)
     {
@@ -129,16 +72,68 @@ public class SqlRepository<TEntity> : ISqlRepository<TEntity>
             _context.SaveChanges();
     }
 
-    public virtual async Task<List<TEntity>> GetAllAsync(bool asNoTracking, CancellationToken cancellationToken)
+    public bool IsExists(Expression<Func<TEntity, bool>> expression)
     {
-        return await (asNoTracking ? TableNoTracking : Entities)
-            .ToListAsync(cancellationToken);
+        return Entities.Any(expression);
     }
 
     #endregion
 
-    public async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+
+    #region Async Method
+    public async virtual Task<TEntity?> GetByIdAsync(CancellationToken cancellationToken, params object[] ids) =>
+        await Entities.FindAsync(ids, cancellationToken);
+    public virtual async Task<List<TEntity>> GetAllAsync(bool asNoTracking, CancellationToken cancellationToken = default) =>
+        await (asNoTracking ? TableNoTracking : Entities).ToListAsync(cancellationToken);
+
+    public virtual async Task AddAsync(TEntity entity, bool saveNow = true, CancellationToken cancellationToken = default)
     {
-        return await Entities.AnyAsync(expression);
+        Assert.NotNull(entity, nameof(entity));
+        await Entities.AddAsync(entity, cancellationToken).ConfigureAwait(false);
+        if (saveNow)
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
     }
+    public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities, bool saveNow = true, CancellationToken cancellationToken = default)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        await Entities.AddRangeAsync(entities, cancellationToken).ConfigureAwait(false);
+        if (saveNow)
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    public virtual async Task UpdateAsync(TEntity entity, bool saveNow = true, CancellationToken cancellationToken = default)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        Entities.Update(entity);
+        if (saveNow)
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+    public virtual async Task UpdateRangeAsync(IEnumerable<TEntity> entities, bool saveNow = true, CancellationToken cancellationToken = default)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        Entities.UpdateRange(entities);
+        if (saveNow)
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity, bool saveNow = true, CancellationToken cancellationToken = default)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        Entities.Remove(entity);
+        if (saveNow)
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+    public virtual async Task DeleteRangeAsync(IEnumerable<TEntity> entities, bool saveNow = true, CancellationToken cancellationToken = default)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        Entities.RemoveRange(entities);
+        if (saveNow)
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    public async Task<bool> IsExistsAsync(Expression<Func<TEntity, bool>> expression, CancellationToken stoppingToken = default)
+    {
+        return await Entities.AnyAsync(expression, stoppingToken);
+    }
+    #endregion
 }

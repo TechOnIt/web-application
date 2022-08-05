d0554c2d76d5a79d96a3ff5698eadd083ea082b6
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-08-05 12:42:50 +0430

    ♻️ Develop command handler body.

--- src/Application/Commands/CommandHandler.cs
+++ src/Application/Commands/CommandHandler.cs
@@ -1,8 +1,34 @@
-﻿using FluentResults;
+﻿namespace iot.Application.Commands;
 
-namespace iot.Application.Commands;
-
-public class CommandHandler<TRequest, TResult>
-    where TResult : IResultBase
+public abstract class CommandHandler<TRequest, TResult> : IRequestHandler<TRequest, TResult>
+    where TRequest : Command<TResult> where TResult : IResultBase, new()
 {
+    #region DI & Ctor
+    protected readonly IMediator _mediator;
+
+    protected CommandHandler(IMediator mediator)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+    #endregion
+
+    public async Task<TResult> Handle(TRequest request, CancellationToken cancellationToken)
+    {
+        TResult result;
+        try
+        {
+            result = await HandleAsync(request, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            // TODO:
+            // Reza
+            // log error.
+            result = new TResult();
+        }
+
+        return result;
+    }
+
+    protected abstract Task<TResult> HandleAsync(TRequest request, CancellationToken cancellationToken);
 }

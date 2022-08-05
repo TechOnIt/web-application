0f0c7e4aa5d307ff29ba50d30ff6745d3cb003c2
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-08-05 12:48:35 +0430

    🐛 bug-fix syntax error: pass configure to service.

--- src/Presentations/Identity.Api/Program.cs
+++ src/Presentations/Identity.Api/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Infrastructure.
-builder.Services.AddDbContextServices();
+builder.Services.AddDbContextServices(builder.Configuration);
 // Logics
 builder.Services.AddApplicationServices();
 builder.Services.AddMediatRServices();

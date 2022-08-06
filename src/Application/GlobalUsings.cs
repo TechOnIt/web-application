ef18449dddb1204089020548a7eb26a5ab2af9b4
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-08-06 19:22:07 +0430

    ♻️ Add general usings to global usings.

--- src/Application/GlobalUsings.cs
+++ src/Application/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using FluentValidation;
 global using iot.Application.Common.Constants;
 global using iot.Domain.Entities.Identity;
 global using MediatR;
+global using iot.Application.Common.Models;
+global using iot.Domain.ValueObjects;
+global using Microsoft.EntityFrameworkCore;

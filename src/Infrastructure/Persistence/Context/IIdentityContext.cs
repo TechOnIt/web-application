08d714c389eff339b1bf1f83fefd90679028f58c
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-08-18 21:28:41 +0430

    🌱 Add Interface for identity context.

--- src/Infrastructure/Persistence/Context/IIdentityContext.cs
+++ src/Infrastructure/Persistence/Context/IIdentityContext.cs
@@ -1,6 +1,16 @@
-﻿namespace iot.Infrastructure.Persistence.Context;
+﻿using iot.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace iot.Infrastructure.Persistence.Context;
 
 public interface IIdentityContext
 {
+    int SaveChanges();
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    DbSet<TEntity> Set<TEntity>() where TEntity : class;
 
+    public DbSet<User> Users { get; set; }
+    public DbSet<LoginHistory> LoginHistories { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
 }

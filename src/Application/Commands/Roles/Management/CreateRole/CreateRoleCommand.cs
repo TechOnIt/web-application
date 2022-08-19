5a63be60c544c6b41fc35ef5633bd027d413bc00
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-08-19 13:14:18 +0430

    ♻️ Refactor role commands, Handlers & Validators.

---
+++ src/Application/Commands/Roles/Management/CreateRole/CreateRoleCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace iot.Application.Commands.Roles.Management.CreateRole;
+
+public class CreateRoleCommand : IRequest<Result<Guid>>, ICommittableRequest
+{
+    public string Name { get; set; }
+}
+
+public class CreateRoleCommandHanlder : IRequestHandler<CreateRoleCommand, Result<Guid>>
+{
+    #region DI & Ctor
+    private readonly IUnitOfWorks _unitOfWorks;
+
+    public CreateRoleCommandHanlder(IUnitOfWorks unitOfWorks)
+    {
+        _unitOfWorks = unitOfWorks;
+    }
+    #endregion
+
+    public async Task<Result<Guid>> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+    {
+        // TODO:
+        // Complete command.
+        return Result.Ok();
+    }
+}
---
+++ src/Application/Commands/Roles/Management/UpdateRole/UpdateRoleCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace iot.Application.Commands.Roles.Management.UpdateRole;
+
+public class UpdateRoleCommandValidator : BaseFluentValidator<UpdateRoleCommand>
+{
+    public UpdateRoleCommandValidator()
+    {
+        RuleFor(r => r.Id)
+            .NotEmpty()
+            .Matches(RegexConstant.Guid)
+            .MaximumLength(100)
+            ;
+
+        RuleFor(r => r.Name)
+            .NotEmpty()
+            .Matches(RegexConstant.EnglishAlphabet)
+            .MaximumLength(50)
+            ;
+    }
+}

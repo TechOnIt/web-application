881c27c8c0819e73c16c88927c9203a54e46dc06
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-04 01:41:47 +0430

    ♻️ Refactor sensor enum.

--- src/Domain/Enums/SensorType.cs
+++ src/Domain/Enums/SensorType.cs
@@ -4,18 +4,13 @@ namespace iot.Domain.Enums;
 
 public class SensorType : Enumeration
 {
-    public static readonly SensorType Temperature = new(1, nameof(Temperature));
-    public static readonly SensorType Earthquake = new(2, nameof(Earthquake));
-    public static readonly SensorType Light = new(3, nameof(Light));
-    public static readonly SensorType EnergyConsumption = new (4, nameof(EnergyConsumption));
+    public static readonly SensorType Thermometer = new(1, nameof(Thermometer)); // 🌡️
+    public static readonly SensorType Psychrometer = new(2, nameof(Psychrometer)); // 💦
+    public static readonly SensorType Earthquake = new(3, nameof(Earthquake)); // ⚠️
+    public static readonly SensorType SmellDetection = new (4, nameof(SmellDetection)); // 👃🏽
 
-    public SensorType()
-    {
-
-    }
+    public SensorType() { }
 
     public SensorType(int id, string name)
-        : base(id, name)
-    {
-    }
+        : base(id, name) { }
 }

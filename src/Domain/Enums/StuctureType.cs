15dc9d2327e02ab80372243386fdbae092904176
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-04 00:30:04 +0430

    ♻️ Cleanup structure type.

--- src/Domain/Enums/StuctureType.cs
+++ src/Domain/Enums/StuctureType.cs
@@ -4,10 +4,9 @@ namespace iot.Domain.Enums;
 
 public class StuctureType : Enumeration
 {
-    public static readonly StuctureType Farm = new(1, nameof(Farm));
-    public static readonly StuctureType Garden = new(2, nameof(Garden));
+    public static readonly StuctureType Home = new(1, nameof(Home));
+    public static readonly StuctureType Agriculture = new(2, nameof(Aviculture));
     public static readonly StuctureType Aviculture = new(3, nameof(Aviculture));
-    public static readonly StuctureType Agriculture = new(4, nameof(Aviculture));
 
     public StuctureType()
     {

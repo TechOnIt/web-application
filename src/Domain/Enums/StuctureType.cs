7d2aa959ca5f3b2ab6a6f825bc7f42cad03fd41a
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-04 01:42:04 +0430

    ♻️ Refactor structure types.

--- src/Domain/Enums/StuctureType.cs
+++ src/Domain/Enums/StuctureType.cs
@@ -4,9 +4,9 @@ namespace iot.Domain.Enums;
 
 public class StuctureType : Enumeration
 {
-    public static readonly StuctureType Home = new(1, nameof(Home));
-    public static readonly StuctureType Agriculture = new(2, nameof(Aviculture));
-    public static readonly StuctureType Aviculture = new(3, nameof(Aviculture));
+    public static readonly StuctureType Home = new(1, nameof(Home)); // 🏡
+    public static readonly StuctureType Agriculture = new(2, nameof(Aviculture)); // 🌱
+    public static readonly StuctureType Aviculture = new(3, nameof(Aviculture)); // 🐔
 
     public StuctureType()
     {

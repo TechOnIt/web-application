02c30f0ad996326a8fb88d143d5a89f6516766a2
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-11-16 20:38:50 +0330

    ♻️ Add default to cancellation token.

--- src/Application/Services/Authenticateion/AuthenticateionContracts/IIdentityService.cs
+++ src/Application/Services/Authenticateion/AuthenticateionContracts/IIdentityService.cs
@@ -5,9 +5,14 @@ namespace iot.Application.Services.Authenticateion.AuthenticateionContracts;
 
 public interface IIdentityService
 {
-    Task<(AccessToken Token, string Message)?> SignInUserAsync(string phoneNumber, string password, CancellationToken cancellationToken = default);
-    Task<(string? Code, string Message)> SendOtpAsync(string phoneNumber, CancellationToken cancellationToken = default);
-    Task<(AccessToken Token, string Message)> SignInUserWithOtpAsync(string otpCode, string phonenumber, CancellationToken cancellationToken = default);
-    Task<(string? Code, string Message)> SignUpAndSendOtpCode(UserViewModel user, CancellationToken cancellationToken);
-    Task<(AccessToken Token, string Message)> SignUpWithOtpAsync(string phonenumber, string otpCode, CancellationToken cancellationToken = default);
+    Task<(AccessToken Token, string Message)?> SignInUserAsync(string phoneNumber, string password,
+        CancellationToken cancellationToken = default);
+    Task<(string? Code, string Message)> SendOtpAsync(string phoneNumber,
+        CancellationToken cancellationToken = default);
+    Task<(AccessToken Token, string Message)> SignInUserWithOtpAsync(string otpCode, string phonenumber,
+        CancellationToken cancellationToken = default);
+    Task<(string? Code, string Message)> SignUpAndSendOtpCode(UserViewModel user,
+        CancellationToken cancellationToken = default);
+    Task<(AccessToken Token, string Message)> SignUpWithOtpAsync(string phonenumber, string otpCode,
+        CancellationToken cancellationToken = default);
 }

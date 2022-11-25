8373f580b5149a913dce8dd357635e3255e4b492
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-11-25 21:01:15 +0330

    🌱 Add paginated list view model. 🍻 Add extension for execute, pagination and select, after queries.

---
+++ src/Application/Common/Extentions/MappingExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace iot.Application.Common.Extentions;
+
+public static class MappingExtensions
+{
+    public static Task<PaginatedList<TOrigin>> PaginatedListAsync<TOrigin>(this IQueryable<TOrigin> queryable,
+        int pageNumber, int pageSize, CancellationToken cancellationToken = default)
+        => PaginatedList<TOrigin>.CreateAsync(queryable, pageNumber, pageSize, cancellationToken);
+
+    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TOrigin, TDestination>(this IQueryable<TOrigin> queryable,
+        int pageNumber, int pageSize, TypeAdapterConfig? config = default, CancellationToken cancellationToken = default)
+        => PaginatedList<TDestination>.CreateAsync<TOrigin, TDestination>(queryable, pageNumber, pageSize,
+            config, cancellationToken);
+}
---
+++ src/Application/Common/Models/PaginatedList.cs
@@ -0,0 +1,57 @@
+﻿namespace iot.Application.Common.Models;
+
+public class PaginatedList<T>
+{
+    #region Ctor's
+    public PaginatedList()
+    {
+    }
+
+    public PaginatedList(List<T> Items, int Count, int PageIndex, int PageSize)
+    {
+        pageIndex = PageIndex;
+        totalPages = (int)Math.Ceiling(Count / (double)PageSize);
+        totalCount = Count;
+        items = Items;
+    }
+    #endregion
+
+    #region Prop's
+    public List<T> items { get; set; }
+    public int pageIndex { get; }
+    public int totalPages { get; }
+    public int totalCount { get; }
+
+    public bool hasPreviousPage => pageIndex > 1;
+    public bool hasNextPage => pageIndex < totalPages;
+    #endregion
+
+    #region Method's
+    public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize, CancellationToken cancellationToken)
+    {
+        pageIndex = pageIndex <= 0 ? 1 : pageIndex;
+        pageSize = pageSize <= 0 ? 5 : pageSize;
+
+        var count = await source.CountAsync();
+        var items = await source
+            .Skip((pageIndex - 1) * pageSize).Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new PaginatedList<T>(items, count, pageIndex, pageSize);
+    }
+
+    public static async Task<PaginatedList<TDestination>> CreateAsync<TOrigin, TDestination>(IQueryable<TOrigin> source,
+        int pageIndex, int pageSize, TypeAdapterConfig? config = default, CancellationToken cancellationToken = default)
+    {
+        pageIndex = pageIndex <= 0 ? 1 : pageIndex;
+        pageSize = pageSize <= 0 ? 5 : pageSize;
+
+        var count = await source.CountAsync();
+        var items = await source
+            .Skip((pageIndex - 1) * pageSize).Take(pageSize)
+            .ProjectToType<TDestination>(config)
+            .ToListAsync(cancellationToken);
+        return new PaginatedList<TDestination>(items, count, pageIndex, pageSize);
+    }
+    #endregion
+}

97fc72d9fc971c0f2d68ac93d53dc446d6626bcc
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-11-26 00:04:11 +0330

    🌱 Add paginated with page skip. 🌱 Add paginated model without skip.

---
+++ src/Application/Common/Models/Paginated.cs
@@ -0,0 +1,28 @@
+﻿namespace iot.Application.Common.Models;
+
+public class Paginated
+{
+    private int _page;
+    public int Page
+    {
+        get { return _page; }
+        set { _page = value < 1 ? 1 : value; }
+    }
+}
+
+public class PaginatedWithSize
+{
+    private int _page;
+    public int Page
+    {
+        get { return _page; }
+        set { _page = value < 1 ? 1 : value; }
+    }
+
+    private int _pageSize;
+    public int PageSize
+    {
+        get { return _pageSize; }
+        set { _pageSize = value < 1 ? 1 : value; }
+    }
+}

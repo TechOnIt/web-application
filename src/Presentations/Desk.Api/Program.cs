c390d044290b8125c324aa568707f3c943118ea5
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-11-27 19:56:00 +0330

    😎 Refactor and cleanup api routing.

--- src/Presentations/Desk.Api/Program.cs
+++ src/Presentations/Desk.Api/Program.cs
@@ -1,11 +1,13 @@
-using GraphQL.Instrumentation;
 using GraphQL.MicrosoftDI;
 using GraphQL.Server;
 using GraphQL.Types;
+using iot.Application;
 using iot.Desk.Api.GraphQl.PerformanceReport;
+using iot.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,17 +28,35 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseCors();
-app.UseAuthorization();
+//app.UseAuthorization();
 // make sure all our schemas registered to route
 app.UseGraphQL<ISchema>();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=General}/{action=Index}/{id?}");
+
+    endpoints.MapControllers();
+});
 
 await app.RunAsync();
 
 
 void ConfigureServices(IServiceCollection services)
 {
+    services.AddInfrastructureServices();
+    services.AddApplicationServices();
+    services.AddFluentValidationServices();
+
     // Add services to the container.
     // add notes schema
     builder.Services.AddSingleton<ISchema, ReportSchema>(services => new ReportSchema(new SelfActivatingServiceProvider(services)));

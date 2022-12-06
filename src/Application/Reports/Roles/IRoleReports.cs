d633428111e8b22a300ea97076016c9f4c1665ff
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-12-06 23:36:09 +0330

    🌱 Add Role Reports service.

---
+++ src/Application/Reports/Roles/IRoleReports.cs
@@ -0,0 +1,7 @@
+﻿namespace iot.Application.Reports.Roles;
+
+public interface IRoleReports
+{
+    Task<PaginatedList<TDestination>> GetAllPaginatedSearchAsync<TDestination>(PaginatedSearchWithSize paginatedSearch,
+        TypeAdapterConfig? config = null, CancellationToken cancellationToken = default);
+}
---
+++ src/Application/Reports/Roles/RoleReports.cs
@@ -0,0 +1,30 @@
+﻿namespace iot.Application.Reports.Roles;
+
+public class RoleReports : IRoleReports
+{
+    #region Ctors
+    private readonly IUnitOfWorks _uow;
+    public RoleReports(IUnitOfWorks unitOfWorks)
+    {
+        _uow = unitOfWorks;
+    }
+    #endregion
+
+    /// <summary>
+    /// Get all roles by search and pagination.
+    /// </summary>
+    /// <typeparam name="TDestination">View model of destination.</typeparam>
+    /// <param name="paginatedSearch">Paging info and keyword for search on role name.</param>
+    public async Task<PaginatedList<TDestination>> GetAllPaginatedSearchAsync<TDestination>(PaginatedSearchWithSize paginatedSearch,
+        TypeAdapterConfig? config = null, CancellationToken cancellationToken = default)
+    {
+        var query = _uow._context.Roles.AsQueryable();
+        #region Search condition
+        if (!string.IsNullOrEmpty(paginatedSearch.Keyword))
+            query = query
+                .Where(q => paginatedSearch.Keyword.Contains(q.Name))
+                .AsQueryable();
+        #endregion
+        return await query.PaginatedListAsync<Role, TDestination>(paginatedSearch.Page, paginatedSearch.PageSize, config, cancellationToken);
+    }
+}

25a85518e7cefb92cacbebad6992fb5cbdf43cb4
ashnoori11 <ashnoori11@gmail.com>
2023-01-06 05:11:18 -0800

    resolve merg conflicts

--- src/Presentations/Identity.Api/Areas/Manage/Controllers/v1/RoleController.cs
+++ src/Presentations/Identity.Api/Areas/Manage/Controllers/v1/RoleController.cs
@@ -8,14 +8,9 @@ using TechOnIt.Application.Queries.Roles.GetAllRoles;
 namespace TechOnIt.Identity.Api.Areas.Manage.Controllers.v1;
 
 [Area("manage")]
-<<<<<<< HEAD
 [ApiController]
 [Route("v1/[controller]/[action]")]
 public class RoleController : ControllerBase
-=======
-[Authorize]
-public class RoleController : BaseController
->>>>>>> b5fee11ae1e31c4c057823a79711086a6a51ad1a
 {
     #region DI & Ctor's
     private readonly IMediator _mediator;
--- src/Presentations/Identity.Api/Areas/Manage/Controllers/v1/UserController.cs
+++ src/Presentations/Identity.Api/Areas/Manage/Controllers/v1/UserController.cs
@@ -3,16 +3,10 @@ using TechOnIt.Application.Queries.Users.GetAllUsers;
 
 namespace TechOnIt.Identity.Api.Areas.Manage.Controllers.v1;
 
-<<<<<<< HEAD
 [ApiController]
 [Area("manage")]
 [Route("v1/[controller]/[action]")]
 public class UserController : ControllerBase
-=======
-[Authorize]
-[Area("manage")]
-public class UserController : BaseController
->>>>>>> b5fee11ae1e31c4c057823a79711086a6a51ad1a
 {
     #region DI & Ctor's
     private readonly IMediator _mediator;

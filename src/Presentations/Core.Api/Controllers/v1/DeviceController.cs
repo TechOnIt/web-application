f26a7e7ee60e5134e45b6029129583c5a44df1a4
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-01-10 19:48:07 +0330

    🌱 Add mock controller.

---
+++ src/Presentations/Core.Api/Controllers/v1/DeviceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechOnIt.Application.Common.Extentions;
+
+namespace TechOnIt.Core.Api.Controllers.v1;
+
+[Authorize]
+[ApiController]
+[Route("v1/[controller]/[action]")]
+public class DeviceController : ControllerBase
+{
+    #region Ctor
+    private readonly IMediator _mediatR;
+    public DeviceController(IMediator mediatR)
+    {
+        _mediatR = mediatR;
+    }
+    #endregion
+
+    #region Commands
+    [HttpGet]
+    public async Task<IActionResult> Command([FromQuery] Guid DeviceId, bool isOn)
+    {
+        return Ok();
+    }
+    #endregion
+
+    #region Queries
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var currentStructure = User.GetCurrentStructureIdAsync();
+        return Ok(currentStructure);
+    }
+    #endregion
+}

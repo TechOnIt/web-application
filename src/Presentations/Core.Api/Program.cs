f47e6dbfc7079fb5a41d08095d4c3c7d4dc365ad
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-01-11 06:59:16 +0330

    ♻️ Add swagger for production mode.

--- src/Presentations/Core.Api/Program.cs
+++ src/Presentations/Core.Api/Program.cs
@@ -43,9 +43,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+}
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 

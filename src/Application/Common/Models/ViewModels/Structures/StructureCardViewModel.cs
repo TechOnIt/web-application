2325e1cdd84b0a02e44951c86152dec36e307681
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-01-14 00:55:07 +0330

    ♻️ Refactor structure card view model.

--- src/Application/Common/Models/ViewModels/Structures/StructureCardViewModel.cs
+++ src/Application/Common/Models/ViewModels/Structures/StructureCardViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace TechOnIt.Application.Common.Models.ViewModels.Structures;
+﻿using TechOnIt.Domain.Enums;
+
+namespace TechOnIt.Application.Common.Models.ViewModels.Structures;
 
 public class StructureCardViewModel
 {
     public string Id { get; set; }
     public string Name { get; set; }
+    public StuctureType Type { get; set; }
+    public string ApiKey { get; set; }
+    public bool IsActive { get; set; }
 }

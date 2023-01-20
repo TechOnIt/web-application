27d05a58d64bffaa39d450f6344b43e035636f8d
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-01-21 00:54:37 +0330

    🚀 Initialize identity ui presentation.

---
+++ src/Presentations/Identity.WebUI/Areas/Manage/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿namespace TechOnIt.Identity.WebUI.Areas.Manage.Controllers;
+
+[Area("Manage")]
+public class UserController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Info()
+    {
+        return View();
+    }
+}

8a51e203456a5297632aac451e426366058c4ec3
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-01-22 00:39:30 +0330

    🌱 Show list of users.

--- src/Presentations/Identity.WebUI/Areas/Manage/Controllers/UserController.cs
+++ src/Presentations/Identity.WebUI/Areas/Manage/Controllers/UserController.cs
@@ -1,12 +1,30 @@
-﻿namespace TechOnIt.Identity.WebUI.Areas.Manage.Controllers;
+﻿using TechOnIt.Application.Common.Models;
+using TechOnIt.Application.Queries.Users.GetAllUsers;
+
+namespace TechOnIt.Identity.WebUI.Areas.Manage.Controllers;
 
 [Area("Manage")]
 public class UserController : Controller
 {
-    [HttpGet]
-    public IActionResult Index()
+    #region Ctor
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
     {
-        return View();
+        _mediator = mediator;
+    }
+    #endregion
+
+    [HttpGet]
+    public async Task<IActionResult> Index(PaginatedSearch paginatedSearch, CancellationToken cancellationToken)
+    {
+        var users = await _mediator.Send(new GetUsersQuery
+        {
+            Page = paginatedSearch.Page,
+            Keyword = paginatedSearch.Keyword
+        }
+        , cancellationToken);
+        return View(users);
     }
 
     [HttpGet]

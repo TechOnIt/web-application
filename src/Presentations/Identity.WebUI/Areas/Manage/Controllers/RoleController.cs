558119d1d2fa73439184b4cdcf586619a0bc9b52
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-01-22 00:16:00 +0330

    🌱 Initialize Dashboard overview & Role index page(MOCK).

---
+++ src/Presentations/Identity.WebUI/Areas/Manage/Controllers/RoleController.cs
@@ -0,0 +1,11 @@
+﻿namespace TechOnIt.Identity.WebUI.Areas.Manage.Controllers;
+
+[Area("Manage")]
+public class RoleController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

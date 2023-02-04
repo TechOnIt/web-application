e5b0b88d9ae4224b9cae510f196ae8c4dc8fb8ea
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-02-04 15:01:42 +0330

    🌱 Add signin page.

---
+++ src/Presentations/Identity.WebUI/Controllers/AuthenticationController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using TechOnIt.Application.Commands.Users.Authentication.SignInCookieCommands;
+
+namespace TechOnIt.Identity.WebUI.Controllers;
+
+public class AuthenticationController : Controller
+{
+    #region Ctor
+    private readonly IMediator _mediator;
+    private readonly IHttpContextAccessor _httpContext;
+
+    public AuthenticationController(IMediator mediator,
+        IHttpContextAccessor httpContext)
+    {
+        _mediator = mediator;
+        _httpContext = httpContext;
+    }
+    #endregion
+
+    [HttpGet]
+    public IActionResult Signin()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Signin(SignInCookieCommand command, CancellationToken cancellationToken)
+    {
+        var isSignedin = await _mediator.Send(command, cancellationToken);
+        if (!isSignedin)
+            return View();
+        return Redirect("/");
+    }
+
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> Signout()
+    {
+        if (_httpContext.HttpContext is not null)
+            await _httpContext.HttpContext
+                .SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        return Redirect("/");
+    }
+}

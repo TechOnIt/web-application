03d28826dcddd9edaa97e56fabe320d7727119e8
ashnoori11 <ashnoori11@gmail.com>
2023-02-11 00:43:54 -0800

    ®️ remove role service - final step

--- src/Application/Services/Authenticateion/AuthenticateionContracts/IRoleService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TechOnIt.Application.Common.Enums.IdentityService;
-
-namespace TechOnIt.Application.Services.Authenticateion.AuthenticateionContracts;
-
-public interface IRoleService
-{
-    Task<(IdentityCrudStatus Result, string Message)> CreateRoleAsync(Role role, CancellationToken cancellationToken = default);
-    Task<IdentityCrudStatus> DeleteRoleByIdAsync(Guid roleId, CancellationToken cancellationToken = default);
-}

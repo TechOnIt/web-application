b84d8adff83be474d7758ab5909ea98df4870f7c
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-04-27 02:33:53 +0430

    🌱 Initialize profile page in dashboard area.

---
+++ src/Presentations/Desk.WebUI/Areas/Dashboard/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿namespace TechOnIt.Desk.WebUI.Areas.Dashboard.Controllers;
+
+[Area("Dashboard")]
+public class ProfileController : Controller
+{
+    private IMediator _mediator;
+
+    public ProfileController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

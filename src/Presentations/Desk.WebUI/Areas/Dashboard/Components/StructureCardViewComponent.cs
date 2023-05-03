fd365425cce9f85565c90342828e82e09cf8c6e3
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-05-03 14:37:17 +0430

    ♻️ Convert structure to view component.

---
+++ src/Presentations/Desk.WebUI/Areas/Dashboard/Components/StructureCardViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using TechOnIt.Application.Common.Models.ViewModels.Structures;
+
+namespace TechOnIt.Desk.WebUI.Areas.Dashboard.Components;
+
+public class StructureCardViewComponent : ViewComponent
+{
+    public StructureCardViewComponent()
+    {
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync(StructureCardViewModel structure) => await Task.FromResult(View(structure));
+}

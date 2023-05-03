14a7736a754010eda2710cea84835dcc4f66c138
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-05-03 16:42:33 +0430

    ♻️ Convert place and device to view component.

--- src/Presentations/Desk.WebUI/Areas/Dashboard/Controllers/DeviceController.cs
+++ src/Presentations/Desk.WebUI/Areas/Dashboard/Controllers/DeviceController.cs
@@ -15,22 +15,22 @@ public class DeviceController : Controller
     #endregion
 
     [HttpGet]
-    public async Task<IActionResult> On(string id, string structureId, CancellationToken cancellationToken)
+    public async Task<IActionResult> On(string deviceId, CancellationToken cancellationToken)
     {
         var result = await _mediator.Send(new DeviceChangeHighStateCommand
         {
-            DeviceId = Guid.Parse(id),
+            DeviceId = Guid.Parse(deviceId),
             IsHigh = true
         }, cancellationToken);
         return Ok();
     }
 
     [HttpGet]
-    public async Task<IActionResult> Off(string id, string structureId, CancellationToken cancellationToken)
+    public async Task<IActionResult> Off(string deviceId, CancellationToken cancellationToken)
     {
         var result = await _mediator.Send(new DeviceChangeHighStateCommand
         {
-            DeviceId = Guid.Parse(id),
+            DeviceId = Guid.Parse(deviceId),
             IsHigh = false
         }, cancellationToken);
         return Ok();

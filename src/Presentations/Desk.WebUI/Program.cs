c43415e6c63ebd9416da5822aa70de5e55216ccc
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-05-12 20:28:29 +0330

    ♻️ Error page for production env & error message for development environment.

--- src/Presentations/Desk.WebUI/Program.cs
+++ src/Presentations/Desk.WebUI/Program.cs
@@ -33,14 +33,20 @@ builder.Services.AddResponseCaching();
 builder.Services.AddProgressiveWebApp();
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseStatusCodePagesWithRedirects("/Home/Error/{0}");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
 app.UseStaticFiles(new StaticFileOptions()

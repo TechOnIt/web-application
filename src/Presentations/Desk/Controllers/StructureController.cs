94863d6af30896d5c6a2bc221835dc77ecf799bc
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-12-01 02:24:28 +0330

    🌱 Add structure index page.

---
+++ src/Presentations/Desk/Controllers/StructureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desk.Controllers
+{
+    public class StructureController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

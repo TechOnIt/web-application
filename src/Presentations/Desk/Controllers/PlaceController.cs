e38c1f4bf26b0bdb1c0f570203941c5c32362fbb
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-12-01 02:30:22 +0330

    🌱 Add place controller refactor layout.

---
+++ src/Presentations/Desk/Controllers/PlaceController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desk.Controllers
+{
+    public class PlaceController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

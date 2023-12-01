1c6e144304ae8edc0d71dab29823b7b49da7bd9a
ashnoori11 <ashnoori11@gmail.com>
2023-12-01 07:43:54 -0800

    add typescript into desk.ui

---
+++ src/Presentations/Desk.WebUI/wwwroot/scripts/interfaces/IHttpService.ts
@@ -0,0 +1,8 @@
+﻿export interface IHttpService {
+    get(url: string): Promise<any>;
+    post(url: string, data: any): Promise<any>;
+    put(url: string, data: any): Promise<any>;
+    delete(url: string): Promise<any>;
+}
+
+
---
+++ src/Presentations/Desk.WebUI/wwwroot/scripts/services/HttpService.ts
@@ -0,0 +1,44 @@
+﻿import { IHttpService } from '../interfaces/IHttpService';
+
+class HttpService implements IHttpService {
+
+    async get(url: string): Promise<any> {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+
+    async post(url: string, data: any): Promise<any> {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const responseData = await response.json();
+        return responseData;
+    }
+
+    async put(url: string, data: any): Promise<any> {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const responseData = await response.json();
+        return responseData;
+    }
+
+    async delete(url: string): Promise<any> {
+        const response = await fetch(url, {
+            method: 'DELETE',
+        });
+        const responseData = await response.json();
+        return responseData;
+    }
+}
+
+export default HttpService;

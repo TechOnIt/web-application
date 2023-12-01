45cacd56e9c304be8f111514e6f156850b37feef
ashnoori11 <ashnoori11@gmail.com>
2023-12-01 07:41:58 -0800

    add signalR and socket management

---
+++ src/Presentations/Desk.WebUI/RealTimeServices/TcpListenerHandler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Net.Sockets;
+using System.Text;
+using TechOnIt.Desk.WebUI.Hubs;
+namespace TechOnIt.Desk.WebUI.RealTimeServices;
+
+public class TcpListenerHandler : BackgroundService
+{
+    private readonly TcpClient _tcpListener;
+    private readonly IHubContext<SensorHub> _hubContext;
+
+    public TcpListenerHandler(IHubContext<SensorHub> hubContext)
+    {
+        _hubContext = hubContext;
+        _tcpListener = new TcpClient("127.0.0.1", 3000);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Task.Run(() => HandleDeviceAsync(), stoppingToken);
+    }
+
+    private async Task HandleDeviceAsync()
+    {
+        NetworkStream stream = _tcpListener.GetStream();
+        while (true)
+        {
+            try
+            {
+                byte[] data = new byte[1024];
+                int length = stream.Read(data, 0, data.Length);
+                string message = Encoding.UTF8.GetString(data, 0, length);
+                await _hubContext.Clients.All.SendAsync(message);
+            }
+            catch
+            {
+                continue;
+            }
+        }
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await base.StopAsync(cancellationToken);
+    }
+}

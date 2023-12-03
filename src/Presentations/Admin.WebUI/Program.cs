387707be63eac5628f9925613ffce692f732455c
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-12-03 18:16:22 +0330

    ♻️ Endpoint - refactoring.

--- src/Presentations/Admin.WebUI/Program.cs
+++ src/Presentations/Admin.WebUI/Program.cs
@@ -64,7 +64,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "areas",
-    pattern: "{area:exists}/{controller=General}/{action=Index}/{id?}");
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");

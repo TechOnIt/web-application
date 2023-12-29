53bca9d346052aa139b30f4e3f1bbd209a080327
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-12-29 13:19:09 +0330

    ♻️ Migration - Change device table to relay.

--- src/Libraries/Infrastructure/Persistence/Migrations/20231229094231_ChangeDeviceToRelay.Designer.cs
+++ src/Libraries/Infrastructure/Persistence/Migrations/20231229094231_ChangeDeviceToRelay.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechOnIt.Infrastructure.Persistence.Context;
 
@@ -11,9 +12,11 @@ using TechOnIt.Infrastructure.Persistence.Context;
 namespace TechOnIt.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20231229094231_ChangeDeviceToRelay")]
+    partial class ChangeDeviceToRelay
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

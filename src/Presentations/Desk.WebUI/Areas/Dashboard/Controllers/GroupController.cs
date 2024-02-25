503178caa82abd7cf384ead7a2d218c043de1997
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2024-02-25 13:45:08 +0330

    🌱 Group controller and menu link.

---
+++ src/Presentations/Desk.WebUI/Areas/Dashboard/Controllers/GroupController.cs
@@ -0,0 +1,22 @@
+﻿namespace TechOnIt.Desk.Web.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class GroupController : Controller
+    {
+        #region DI
+
+        private readonly IMediator _mediator;
+        public GroupController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        #endregion
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
